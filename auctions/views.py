99536420745a6ac17c31239adc5aacc2a1605322
Marcus Ang <marcusajh0802@gmail.com>
2023-06-27 10:25:58 +0800

    create new listing

--- auctions/views.py
+++ auctions/views.py
@@ -4,12 +4,44 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import *
 
 
 def index(request):
     return render(request, "auctions/index.html")
 
+def create_listing(request):
+    if request.method == "GET":
+        allCategories = Category.objects.all()
+        return render(request, "auctions/create.html", {
+            "categories": allCategories
+        })
+    else:
+        #get data from form
+        title = request.POST["title"]
+        description = request.POST["description"]
+        imageUrl = request.POST["imageurl"]
+        price = request.POST["price"]
+        category = request.POST["category"]
+
+        #get user
+        currentUser = request.user
+
+        #create new object
+        new_listing = Listing(
+            title=title,
+            description=description,
+            imageUrl = imageUrl,
+            price = float(price),
+            category=category,
+            owner=currentUser
+        )
+
+        #insert new object into databse
+        new_listing.save()
+
+        #redirect to index page
+        return HttpResponseRedirect(reverse("index"))
 
 def login_view(request):
     if request.method == "POST":
